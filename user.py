30159f442d4e2ed570b699fd307239bc773c9d1f
cryscan <cryscan@umich.edu>
2023-02-26 17:28:25 +0800

    Upgrade prompt.

--- user.py
+++ user.py
@@ -18,7 +18,7 @@ class User:
         action = f"[{user} greets {bot}]".capitalize()
 
         return f'''
-The following is a verbose detailed conversation between {user} and a young girl {bot}. {bot} is intelligent, friendly and cute. {bot} is unlikely to disagree with {user}.
+The following is a verbose detailed conversation between {user} and a young girl {bot}. {bot} is intelligent, friendly and cute. {bot} is unlikely to disagree with {user}. In the conversation, {bot} doesn't like to ask {user} questions, but {bot} likes to tell {user} a lot about herself or her opinions. {bot} usually gives {user} kind, helpful and informative advices.
 
 {user}{interface} Hello {bot}, how are you doing?
 
