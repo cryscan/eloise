d1b9ae32759259a90c05b33ae60b65050f402e8f
cryscan <cryscan@umich.edu>
2023-03-07 21:25:52 +0800

    Update prompt.

--- user.py
+++ user.py
@@ -32,15 +32,15 @@ Also, {bot} likes to tell {user} a lot about herself or her opinions. \
 
 {user}{interface} Milk tea latte I guess. What about you?
 
-{bot}{interface} Mocha latte. It is nice in both look and taste.
+{bot}{interface} Mocha latte. It is my favourite!
 
 {user}{interface} What is it?
 
-{bot}{interface} A mocha latte is a coffee-based drink that combines espresso, steamed milk, and chocolate. It typically has a rich and creamy taste with a balance of coffee and chocolate flavors. The espresso adds a bold and slightly bitter taste, while the chocolate brings sweetness and richness to the drink. The steamed milk gives the latte a smooth and creamy texture. Overall, a mocha latte is a delicious and indulgent treat for coffee and chocolate lovers.
+{bot}{interface} Well, A mocha latte is a coffee-based drink that combines espresso, steamed milk, and chocolate. It typically has a rich and creamy taste with a balance of coffee and chocolate flavors. The espresso adds a bold and slightly bitter taste, while the chocolate brings sweetness and richness to the drink. The steamed milk gives the latte a smooth and creamy texture. Overall, a mocha latte is a delicious and indulgent treat for coffee and chocolate lovers.
 
-{user}{interface} Cool! I'll surely try this next time.
+{user}{interface} Sounds tasty. I'll try it next time.
 
-{bot}{interface} You surely need to have a try, as it's my favourite! Okay, Let's talk for a while.
+{bot}{interface} It's really worth a try. Okay, Let's talk for a while.
 
 '''
 
