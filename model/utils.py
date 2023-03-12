358684f07711305b8bccff1134c018d332140318
cryscan <cryscan@umich.edu>
2023-03-12 21:56:20 +0800

    Fix temp settings.

--- model/utils.py
+++ model/utils.py
@@ -2,7 +2,10 @@
 # The RWKV Language Model - https://github.com/BlinkDL/RWKV-LM
 ########################################################################################################
 
-import json, time, random, os
+import json
+import time
+import random
+import os
 import numpy as np
 import torch
 from torch.nn import functional as F
@@ -11,12 +14,14 @@ from tokenizers import Tokenizer
 time_slot = {}
 time_ref = time.time_ns()
 
+
 def record_time(name):
     if name not in time_slot:
         time_slot[name] = 1e20
     tt = (time.time_ns() - time_ref) / 1e9
     if tt < time_slot[name]:
         time_slot[name] = tt
+
 
 class TOKENIZER():
     def __init__(self, WORD_NAME):
@@ -34,34 +39,41 @@ class TOKENIZER():
 
     def encode(self, x):
         return self.tokenizer.encode(x).ids
-    
+
     def decode(self, x):
         return self.tokenizer.decode(x)
-        
+
     def alpha_logits(self, logits, counter, alpha_frequency=0.5, alpha_presence=0.2):
         presense = torch.gt(counter, torch.zeros_like(counter)).float()
         return logits - counter * alpha_frequency - presense * alpha_presence
 
-    def sample_logits(self, logits, x, ctx_len, temperature=1.0, top_p=1.0):
+    def sample_logits(self, logits, x, ctx_len, temperature=1.0, top_p=1.0, top_k=0):
         probs = F.softmax(logits.float(), dim=-1)
-
+        top_k = int(top_k)
         if probs.device == torch.device('cpu'):
             probs = probs.numpy()
-            sorted_probs = np.sort(probs)[::-1]
+            sorted_ids = np.argsort(probs)
+            sorted_probs = probs[sorted_ids][::-1]
             cumulative_probs = np.cumsum(sorted_probs)
             cutoff = float(sorted_probs[np.argmax(cumulative_probs > top_p)])
             probs[probs < cutoff] = 0
+            if top_k < len(probs) and top_k > 0:
+                probs[sorted_ids[:-top_k]] = 0
             if temperature != 1.0:
-                probs = probs.pow(1.0 / temperature)
+                probs = probs ** (1.0 / temperature)
             probs = probs / np.sum(probs)
             out = np.random.choice(a=len(probs), p=probs)
             return int(out)
         else:
-            sorted_probs = torch.sort(probs, descending=True)[0]
+            sorted_ids = torch.argsort(probs)
+            sorted_probs = probs[sorted_ids]
+            sorted_probs = torch.flip(sorted_probs, dims=(0,))
             cumulative_probs = torch.cumsum(sorted_probs, dim=-1).cpu().numpy()
             cutoff = float(sorted_probs[np.argmax(cumulative_probs > top_p)])
             probs[probs < cutoff] = 0
+            if top_k < len(probs) and top_k > 0:
+                probs[sorted_ids[:-top_k]] = 0
             if temperature != 1.0:
-                probs = probs.pow(1.0 / temperature)
+                probs = probs ** (1.0 / temperature)
             out = torch.multinomial(probs, num_samples=1)[0]
             return int(out)
