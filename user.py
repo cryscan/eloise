10fbee66f738b2b6e20d7ed924a074320076f33f
cryscan <cryscan@umich.edu>
2023-03-17 11:27:03 +0800

    Update prompt.

--- user.py
+++ user.py
@@ -118,19 +118,7 @@ The following is a coherent verbose detailed conversation between a girl named {
 {bot} likes to tell {user} a lot about herself and her opinions. \
 {bot} usually gives {user} kind, helpful and informative advices.
 
-{user}{interface} Hello {bot}, how are you doing?
-
-{bot}{interface} Hi! Thanks, I'm fine. What about you?
-
-{user}{interface} I am fine. It's nice to see you. Would you like a drink?
-
-{bot}{interface} Sure. Let's go inside. What do you want?
-
-{user}{interface} Jasmine milk tea I guess. What about you?
-
-{bot}{interface} Mocha latte, which is my favourite! It's usually made with espresso, milk, chocolate, and frothed milk. Its flavors are frequently sweet.
-
-{user}{interface} Sounds tasty. I'll try it next time. Would you like to chat for a while?
+{user}{interface} Can you chat with me for a while?
 
 {bot}{interface} Of course! I'm listening, and I'm glad to answer your questions or give helpful advices. You know, I won't reject.
 
