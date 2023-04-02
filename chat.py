6bcfd041ed1f481224d293e7320e54e72e462195
cryscan <cryscan@umich.edu>
2023-04-03 01:03:59 +0800

    Update to raven v6.

--- chat.py
+++ chat.py
@@ -44,7 +44,7 @@ AVOID_REPEAT = '，。：？！'
 MAX_MESSAGE_LEN = 4096
 CHUNK_LEN = 128
 
-MAX_GENERATE_LEN = 150
+MAX_GENERATE_LEN = 250
 MAX_REPLY_LEN = 1024
 
 args = types.SimpleNamespace()
@@ -62,7 +62,7 @@ args.strategy = 'cuda fp16i8 *20 -> cuda fp16'
 # args.MODEL_NAME = '/root/autodl-tmp/models/RWKV-4-Pile-14B-20230213-8019'
 # args.MODEL_NAME = '/root/autodl-tmp/models/RWKV-4-Pile-14B-20230228-ctx4096-test663'
 # args.MODEL_NAME = '/root/autodl-tmp/models/RWKV-4-Pile-14B-20230313-ctx8192-test1050'
-args.MODEL_NAME = '/root/autodl-tmp/models/RWKV-4-Pile-14B-Instruct-test5-20230329-ctx4096.pth'
+args.MODEL_NAME = '/root/autodl-tmp/models/RWKV-4-Raven-14B-v6-EngChnJpn-20230401-ctx4096'
 # args.MODEL_NAME = '/root/autodl-tmp/models/RWKV-4-Pile-7B-EngChn-test5-20230330'
 
 args.STATE_DUMP_NAME = './state_14b'
